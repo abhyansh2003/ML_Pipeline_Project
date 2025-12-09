c0891de929e9b6d192f8de3e7f094994f02fba62
Abhyansh Shrivastava <201B013@juetguna.in>
2025-12-09 18:33:21 +0530

    I updated the prediction_pipeline py file with adding some classes and functions.

--- src/utils.py
+++ src/utils.py
@@ -19,6 +19,13 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_objt:
+            return pickle.load(file_objt)
+    except Exception as e:
+        raise CustomException(e, sys)
+
 def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
